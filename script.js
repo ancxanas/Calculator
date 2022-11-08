33bfd2e44d8385b8ebb94e01ee2c399f09556bff
ancxanas <anas.corona.github@gmail.com>
2022-11-08 15:24:46 +0530

    Added functions for handling operations and updating display

--- script.js
+++ script.js
@@ -10,15 +10,64 @@ function operate(operator, a, b) {
   switch (operator) {
     case '+':
       return add(a, b);
-      break;
     case '-':
       return subtract(a, b);
-      break;
     case '*':
       return multiply(a, b);
-      break;
     case '/':
       return divide(a, b);
-      break;
   }
 }
+
+const previousOperandTextElement = document.querySelector('.previous-operand');
+const currentOperandTextElement = document.querySelector('.current-operand');
+const numberButtons = document.querySelectorAll('.number-button');
+const operationButtons = document.querySelectorAll('.operation-button');
+const allClearButton = document.querySelector('.all-clear-button');
+const deleteButton = document.querySelector('.delete-button');
+const decimalButton = document.querySelector('.decimal-button');
+const equalsButton = document.querySelector('.equals-button');
+
+let displayValue = '';
+let firstNumber = '';
+let nextNumber = '';
+let result = '';
+let operator = undefined;
+let isOperatorPressed = false;
+
+numberButtons.forEach((number) => {
+  number.addEventListener('click', updateDisplay);
+});
+
+function updateDisplay(e) {
+  displayValue = currentOperandTextElement.innerText += e.target.textContent;
+  firstNumber = previousOperandTextElement.innerText.replace(/\D/g, '');
+  nextNumber = currentOperandTextElement.innerText;
+}
+
+operationButtons.forEach((operator) => {
+  operator.addEventListener('click', handleOperation);
+});
+
+function handleOperation(e) {
+  previousOperandTextElement.innerText += displayValue += e.target.textContent;
+  operator = e.target.textContent;
+  currentOperandTextElement.innerText = '';
+}
+
+equalsButton.addEventListener('click', () => {
+  result = operate(operator, firstNumber, nextNumber);
+  previousOperandTextElement.innerText += displayValue;
+  currentOperandTextElement.innerText = '';
+  currentOperandTextElement.innerText += result;
+  previousOperandTextElement.innerHTML = '';
+  previousOperandTextElement.innerText += firstNumber += operator += nextNumber;
+});
+
+allClearButton.addEventListener('click', () => {
+  firstNumber = '';
+  nextNumber = '';
+  operator = '';
+  previousOperandTextElement.innerText = '';
+  currentOperandTextElement.innerText = '';
+});
